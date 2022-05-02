1ee4a0bd0a5c50c637b7ab512c6402491e0a6990
Abraham Saavedra <abrasm92@gmail.com>
2022-05-02 21:33:12 +0200

    Addet listpage styles and browser router

---
+++ pages/ListPageStyles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ListPageStyles = styled.div`
+  background-color: #11001c;
+`;
+
+export default ListPageStyles;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppStyles from "./AppStyles";
 import Header from "./components/Header/Header";
-import MainDrinkFilter from "./components/MainDrinkFilter/MainDrinkFilter";
 import Navbar from "./components/Navbar/Navbar";
-
 
 function App() {
   return (
     <>
       <AppStyles />
       <Header />
-      <MainDrinkFilter
-      text={"Non-alcoholic"}
-      image="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
-    />
+      <BrowserRouter>
+        <Routes>
+          <Route path="ListPage" />
+        </Routes>
+      </BrowserRouter>
       <Navbar />
-
     </>
   );
 }
