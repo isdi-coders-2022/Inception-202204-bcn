1032187b5b326aecff9f6edd084513c9356ed7dc
Alejandro Gomez <jandrogo@gmail.com>
2022-05-02 19:50:27 +0200

    Added component MainDrinkFilter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import MainDrinkFilter from "./components/MainDrinkFilter/MainDrinkFilter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MainDrinkFilter
+          text={"Non-alcoholic"}
+          image="https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
+        />
       </header>
     </div>
   );
