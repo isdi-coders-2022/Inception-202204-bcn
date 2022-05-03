4efa7f92ca9c61fa256fa06e9a62f66c9a165c3a
NatBernat <76127144+NatBernat@users.noreply.github.com>
2022-05-03 12:26:54 +0200

    Fixed no used var deleting import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import AppStyles from "./AppStyles";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import ListPage from "./pages/ListPage/ListPage";
 
 
 function App() {
