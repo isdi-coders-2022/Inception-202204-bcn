cdf32227b63d3caee678e8595075cb869111c634
Abraham Saavedra <abrasm92@gmail.com>
2022-05-03 13:59:21 +0200

    fix sonarcloud and pagination html and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import AppStyles from "./AppStyles";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 
-
 function App() {
   return (
     <>
---
+++ src/components/Pagination/Pagination.styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const PaginationStyles = styled.div`
+  color: #fff;
+  display: flex;
+  color: #fff;
+  align-items: center;
+  justify-content: space-evenly;
+  img {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+export default PaginationStyles;
