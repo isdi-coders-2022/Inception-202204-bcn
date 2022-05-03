ab93c83b4a3750dc2c05468224c4e43c8b81a2a0
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-03 21:57:36 +0200

    Created DetailPage component style file, also linked to main component

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,10 +1,11 @@
 import Detail from "../../components/Detail/Detail";
+import DetailPageStyled from "./DetailPageStyled";
 
 const DetailPage = ({ drink, action1, action2 }) => {
   return (
-    <>
+    <DetailPageStyled>
       <Detail drink={drink} action1={action1} action2={action2} />
-    </>
+    </DetailPageStyled>
   );
 };
 
---
+++ src/pages/DetailPage/DetailPageStyled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const DetailPageStyled = styled.div`
+  margin: 0;
+`;
+
+export default DetailPageStyled;
