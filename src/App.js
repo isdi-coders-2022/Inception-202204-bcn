a7c942a76ca3116858cb09de11cac9a6b8489bfa
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-03 18:25:01 +0200

    Set up app page to define Detail style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,30 @@ import Navbar from "./components/Navbar/Navbar";
 import MainPage from "./pages/MainPage/MainPage";
 import ListPage from "./pages/ListPage/ListPage";
 import { Route, Routes } from "react-router-dom";
+import Detail from "./components/Detail/Detail";
+
+const drink = {
+  idDrink: "11007",
+  strDrink: "Margarita",
+  strDrinkAlternate: null,
+  strTags: "IBA,ContemporaryClassic",
+  strVideo: null,
+  strCategory: "Ordinary Drink",
+  strIBA: "Contemporary Classics",
+  strAlcoholic: "Alcoholic",
+  strGlass: "Cocktail glass",
+  strInstructions:
+    "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
+  strDrinkThumb:
+    "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+  strIngredient1: "Tequila",
+  strIngredient2: "Triple sec",
+  strIngredient3: "Lime juice",
+  strIngredient4: "Salt",
+  strIngredient5: null,
+  strIngredient6: null,
+  strIngredient7: null,
+};
 
 function App() {
   return (
@@ -14,6 +38,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/main-page" element={<MainPage />} />
+        <Route path="/detail-page" element={<Detail drink={drink} />} />
         <Route
           path="/list-page"
           element={
