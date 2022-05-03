602a63aa77dd1d84f62a54f91cf9b6cd3ee1c261
Alejandro Gomez <jandrogo@gmail.com>
2022-05-03 15:32:24 +0200

    Added basic test Formulary component

--- src/components/Formulary/FormularyStyled.js
+++ src/components/Formulary/FormularyStyled.js
@@ -7,16 +7,20 @@ const FormularyStyled = styled.form`
   align-items: center;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   gap: 20px;
-  margin-bottom: 150px;
+  margin-bottom: 100px;
+  margin-top: 20px;
 
   input,
-  select,
-  textarea {
+  select {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
     width: 240px;
     height: 30px;
+  }
+  textarea {
+    width: 240px;
+    height: 100px;
   }
 `;
 
