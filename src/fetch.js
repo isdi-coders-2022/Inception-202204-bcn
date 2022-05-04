479a7c969b6780ffb20750849901e986c68f1ea7
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-04 11:28:29 +0200

    Fixed naming and paths code smells, also duplicated await on fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -3,7 +3,7 @@ const Fetch = () => {
     const response = await fetch(
       `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`
     );
-    return await response.json();
+    return response.json();
   };
   getDrinkList();
 };
