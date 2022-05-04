904ecb1545479424aee35d19979ec41962da2daa
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-04 09:39:25 +0200

    Fixed remaining imports from App.js

--- src/App.js
+++ src/App.js
@@ -6,30 +6,6 @@ import MainPage from "./pages/MainPage/MainPage";
 import ListPage from "./pages/ListPage/ListPage";
 import { Route, Routes } from "react-router-dom";
 import FormPage from "./pages/FormPage/FormPage";
-import DetailPage from "./pages/DetailPage/DetailPage";
-
-const drink = {
-  idDrink: "11007",
-  strDrink: "Margarita",
-  strDrinkAlternate: null,
-  strTags: "IBA,ContemporaryClassic",
-  strVideo: null,
-  strCategory: "Ordinary Drink",
-  strIBA: "Contemporary Classics",
-  strAlcoholic: "Alcoholic",
-  strGlass: "Cocktail glass",
-  strInstructions:
-    "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.",
-  strDrinkThumb:
-    "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
-  strIngredient1: "Tequila",
-  strIngredient2: "Triple sec",
-  strIngredient3: "Lime juice",
-  strIngredient4: "Salt",
-  strIngredient5: null,
-  strIngredient6: null,
-  strIngredient7: null,
-};
 
 function App() {
   return (
