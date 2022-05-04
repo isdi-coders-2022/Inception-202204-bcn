9a7ec434f1c27c4356848f4af4ddc88ddb847986
Alejandro Gomez <jandrogo@gmail.com>
2022-05-04 15:26:40 +0200

    Added drinkActionCretor.js, ans modified drinkActionTypes.js

---
+++ src/store/actions/drinkActionCreators.js
@@ -0,0 +1,26 @@
+import {
+  addDrinkActionType,
+  deleteDrinkActionType,
+  editDrinkActionType,
+  loadDrinkActionType,
+} from "./drinkActionTypes";
+
+export const addDrinkActionCreation = (id) => ({
+  type: addDrinkActionType,
+  id,
+});
+
+export const deleteDrinkActionCreation = (id) => ({
+  type: deleteDrinkActionType,
+  id,
+});
+
+export const editDrinkActionCreation = (id) => ({
+  type: editDrinkActionType,
+  id,
+});
+
+export const loadDrinkActinCreation = (id) => ({
+  type: loadDrinkActionType,
+  id,
+});
