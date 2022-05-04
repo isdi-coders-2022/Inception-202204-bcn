6723207253e486862b39290be011c66f616199a5
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-04 10:32:16 +0200

    Added test for DetailPage component

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -3,7 +3,7 @@ import DetailPageStyled from "./DetailPageStyled";
 
 const DetailPage = ({ drink, action1, action2 }) => {
   return (
-    <DetailPageStyled className="detail-page">
+    <DetailPageStyled>
       <Detail drink={drink} action1={action1} action2={action2} />
     </DetailPageStyled>
   );
