434786f2f7dbf599c4746d91e96d00ec6b93343f
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-05 09:33:54 +0200

    Fixed code smells on ListPage and AppProvider

--- src/components/MainDrinkFilter/MainDrinkFilter.js
+++ src/components/MainDrinkFilter/MainDrinkFilter.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import MainDrinkFilterStyles from "./MainDrinkFilterStyled";
+import MainDrinkFilterStyled from "./MainDrinkFilterStyled";
 
 const MainDrinkFilter = ({ text, image }) => {
   return (
-    <MainDrinkFilterStyles>
+    <MainDrinkFilterStyled>
       <Link to="list-page">
         <div>
           <img src={image} alt={`link to ${text} cocktails`} />
@@ -12,7 +12,7 @@ const MainDrinkFilter = ({ text, image }) => {
           </div>
         </div>
       </Link>
-    </MainDrinkFilterStyles>
+    </MainDrinkFilterStyled>
   );
 };
 
