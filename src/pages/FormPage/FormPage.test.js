146727d9880070503ccee767d02163571927a2fa
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-07 19:30:56 +0200

    Fixed renderer import on snapshot FormPage test

--- src/pages/FormPage/FormPage.test.js
+++ src/pages/FormPage/FormPage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FormPage from "./FormPage";
-import TestRenderer from "react-test-renderer";
+import Renderer from "react-test-renderer";
 import "@testing-library/jest-dom";
 import AppProvider from "../../store/context/AppProvider";
 import AppProviderLocal from "../../store/context/AppProviderLocal";
@@ -8,7 +8,7 @@ import AppProviderLocal from "../../store/context/AppProviderLocal";
 describe("Given FormPage component", () => {
   describe("When rendered", () => {
     test("Then it should always match this snapshot", () => {
-      const formularyPage = TestRenderer.create(
+      const formularyPage = Renderer.create(
         <AppProvider>
           <AppProviderLocal>
             <FormPage />
